a0299a7f03c26e8199708aacb7076068a05b5b9e
Chris <Chris@MaryJanes-MacBook-Pro.local>
2017-11-21 19:25:31 -0700

    first commit

---
+++ AwM_function.js
@@ -0,0 +1,18 @@
+var mainVm = new Vue({
+    el: '#app',
+    data : {
+        flagArray: []
+    },
+    methods: {
+        placeAFlag: function($event) {
+            this.flagArray.push({
+                pageX: event.offsetX + 'px',
+                pageY: event.offsetY + 'px',
+            })
+        },
+        removeFlag: function(flags, index, $event) {
+            event.stopPropagation()
+            this.flagArray.splice(index, 1)
+        }
+    }
+});
